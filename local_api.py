fc2a9c4329f34c444ae50d26fdf8c9a0735e5cbf
vlord1 <vlord1@wgu.edu>
2026-07-09 04:11:04 +0000

    Complete Step 5: Implement FastAPI RESTful API and local testing

--- local_api.py
+++ local_api.py
@@ -3,14 +3,12 @@ import json
 import requests
 
 # TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+r = requests.get("http://127.0.0.1:8000/")
 
 # TODO: print the status code
-# print()
+print(f"GET Status Code: {r.status_code}")
 # TODO: print the welcome message
-# print()
-
-
+print(f"GET Welcome Message: {r.json()}")
 
 data = {
     "age": 37,
@@ -30,9 +28,9 @@ data = {
 }
 
 # TODO: send a POST using the data above
-r = None # Your code here
+r = requests.post("http://127.0.0.1:8000/data/", json=data)
 
 # TODO: print the status code
-# print()
+print(f"POST Status Code: {r.status_code}")
 # TODO: print the result
-# print()
+print(f"POST Result: {r.json()}")
